761a579dbd65fbdbc90ac2861c922d9ae714e853
Jakapob Namon <jakan729@wisva.postbox.in.th>
2021-02-17 22:17:09 +0700

    init movie

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import Movie from './components/Movie';
+
+const MOVIE_API = 'https://api.themoviedb.org/3/search/movie?api_key=99f368f25199dd4b1f91ed36b077238d&query=a'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=99f368f25199dd4b1f91ed36b077238d&query='
+
 function App() {
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(async () => {
+    getMovies(MOVIE_API)
+  }, []);
+
+  const getMovies = (API) => {
+    fetch(API).then(res => res.json()).then(data => {
+      setMovies(data.results);
+    });
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm && searchTerm !== ' ') {
+      getMovies(SEARCH_API + searchTerm);
+      setSearchTerm('');
+    }
+  }
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="search"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleOnChange} />
+        </form>
       </header>
-    </div>
+      <div className="movie-container">
+        {movies.length > 0 && movies.map((movie) =>
+          <Movie key={movie.id} {...movie} />
+        )}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Movie.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Img from '../movie_icon.png';
+
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+const setVoteClass = (vote) => {
+    if (vote >= 8) {
+        return 'green';
+    } else if (vote >= 5) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+}
+
+function Movie({ title, poster_path, overview, vote_average }) {
+    return (
+        <div className="movie">
+            <img src={poster_path ? IMG_API + poster_path : Img} alt={title} />
+
+            <div className="movie-info">
+                <h3>{title}</h3>
+                <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
+            </div>
+
+            <div className="movie-overview">
+                <h2>Overview:</h2>
+                <p>{overview}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Movie
